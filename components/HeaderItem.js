a5854a43d51ad7a9b2b3ba14a5852f24e62b9678
Ayo <bimboladxtreme@gmail.com>
2021-08-18 13:09:57 +0100

    Header is complete

---
+++ components/HeaderItem.js
@@ -0,0 +1,17 @@
+import React from "react";
+import styled from "styled-components";
+
+function HeaderItem({ Icon, title }) {
+	return (
+		<Div className=" flex flex-col items-center cursor-pointer w-12 sm:w-20 hover:text-white group">
+			<Icon className="h-8 mb-1 group-hover:animate-bounce" />
+			<p className="tracking-widest opacity-0 group-hover:opacity-100">
+				{title}{" "}
+			</p>
+		</Div>
+	);
+}
+
+export default HeaderItem;
+
+const Div = styled.div``;
